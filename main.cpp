95a851f9a61e7e8419c5009a0e670c989015f90a
httpmail <httpmail@sina.com.cn>
2018-05-11 15:14:34 +0800

    Revert "PG::State implement"
    This reverts commit 43ce37198a4334c9c5adbe2305e4cc34a1517475.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,12 @@
 int main()
 {
     PG::Log::Instance().Initilize();
+
+    int cout = 10;
+    while (cout--)
+    {
+        PG_LOG_INFO("abcd", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx %d", cout);
+    }
+    std::cout << "quit " << std::endl;
     return 0;
 }
